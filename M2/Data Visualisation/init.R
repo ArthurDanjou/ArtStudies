85a74691959887ece4c06e93a3682e08ba62a123
Arthur DANJOU <arthurdanjou@outlook.fr>
2025-10-14 11:16:14 +0200

    Réorganisation du code pour améliorer la lisibilité et la structure de l'installation des packages

--- M2/Data Visualisation/init.R
+++ M2/Data Visualisation/init.R
@@ -1,25 +1,24 @@
 # Liste des packages à installer
 packages_to_install <- c(
-    "lattice", "grid", "ggplot2", "gridExtra", "locfit", "scales",
-    "formattable", "RColorBrewer", "plotly", "dplyr", "tidyr",
-    "rmarkdown", "ggthemes", "cowplot", "kableExtra", "ggridges",
-    "colorspace", "sf", "mapview", "tidyverse", "readxl", "readr",
-    "giscoR", "gapminder", "GGally", "ggfortify", "lubridate", "zoo",
-    "xts", "forecast", "feasts", "tseries", "tsibble", "fable"
+  "lattice", "grid", "ggplot2", "gridExtra", "locfit", "scales",
+  "formattable", "RColorBrewer", "plotly", "dplyr", "tidyr",
+  "rmarkdown", "ggthemes", "cowplot", "kableExtra", "ggridges",
+  "colorspace", "sf", "mapview", "tidyverse", "readxl", "readr",
+  "giscoR", "gapminder", "GGally", "ggfortify", "lubridate", "zoo",
+  "xts", "forecast", "feasts", "tseries", "tsibble", "fable"
 )
 
 # Fonction pour installer les packages manquants
 install_if_absent <- function(package_name) {
-    if (!requireNamespace(package_name, quietly = TRUE)) {
-        install.packages(package_name)
-        message(paste("Package", package_name, "installé avec succès."))
-    } else {
-        message(paste("Package", package_name, "déjà installé, installation ignorée."))
-    }
+  if (!requireNamespace(package_name, quietly = TRUE)) {
+    install.packages(package_name)
+    message(paste("Package", package_name, "installé avec succès."))
+  } else {
+    message(
+      paste("Package", package_name, "déjà installé, installation ignorée.")
+    )
+  }
 }
 
 # Appliquer la fonction à la liste de packages
 lapply(packages_to_install, install_if_absent)
-
-# Note : Une fois les packages installés, n'oubliez pas de les charger dans votre session R
-# avec la commande library(). Par exemple, library(tidyverse).
